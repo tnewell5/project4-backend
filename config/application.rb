9b1846a8ba1abd40f1b5fca234e620d0b4571f33
Tanya Newell <trozova5@gmail.com>
2016-05-03 13:46:40 -0400

    still htts settins

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Project4Backend
   class Application < Rails::Application
-    config.force_ssl = true
+    config.force_ssl = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
